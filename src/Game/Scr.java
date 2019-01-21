0a10b899e6f1825b799a8eb8c18f3217073154e8
armen <armen@10.154.40.121>
2019-01-21 14:03:24 +0100

    reryrye

--- src/Game/Scr.java
+++ src/Game/Scr.java
@@ -8,8 +8,8 @@ public class Scr {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
-		// br.close();gfgd
+		// br.close();gfgd jksdjflsjkfjsdljfdsk jskdjf
 		return str;
-
+		
 	}
 }
