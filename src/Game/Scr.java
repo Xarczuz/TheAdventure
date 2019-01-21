0e82dd57f9dde8c6ee713f054e765ad166383531
armen <armen@10.154.40.121>
2019-01-21 14:05:38 +0100

    rad 11 ändrad

--- src/Game/Scr.java
+++ src/Game/Scr.java
@@ -8,7 +8,7 @@ public class Scr {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
-		// br.close();gfgd jksdjflsjkfjsdljfdsk jskdjf
+		br.close();
 		return str;
 		
 	}
