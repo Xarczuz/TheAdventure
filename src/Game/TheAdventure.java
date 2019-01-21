8d1467941f7464f546257d6fc47a50c925312ab6
armen <armen@10.154.40.121>
2019-01-21 14:37:11 +0100

    class The Adventure

--- src/Game/TheAdventure.java
+++ src/Game/TheAdventure.java
@@ -1,10 +1,15 @@
 package Game;
 
+import java.io.IOException;
 
 public class TheAdventure {
 
-	public static void main(String[] args) {
-			
+	public static void main(String[] args) throws IOException {
+		
+		System.out.print("The Adventure Quiz Game!\n");
+		String tmp = Scr.inStr();
+		
+		System.out.println(tmp);
 	}
 
 }
