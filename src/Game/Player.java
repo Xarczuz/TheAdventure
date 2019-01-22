a6e5c1cc0df9392239ca766830218aa2a290f907
Chjun-chi Chiu <chjunchi.chiu@gmail.com>
2019-01-22 11:09:45 +0100

    Update

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -5,13 +5,13 @@ public class Player {
 	private String name;
 	private int player_score, player_lives;
 	
-	public Player(String name, int player_score, int player_lives) {
-		this.name = name;
-		this.player_score = player_score;
-		this.player_lives = player_lives;
+	public Player() {
+		
+		this.player_score = 0;
+		this.player_lives = 3;
 	}
 	
-	public void player_stats(String name, int player_score, int player_lives) {
+	public void player_stats() {
 		System.out.println("Player -> " + name);
 		System.out.println("Your Points: " + player_score);
 		System.out.println("Your lives: " + player_lives);
--- src/Game/TheAdventure.java
+++ src/Game/TheAdventure.java
@@ -8,15 +8,22 @@ public class TheAdventure {
 		
 		System.out.print("The Adventure Quiz Game!\n");
 		String tmp = "";
+		//Generate Questions
 		Quiz_Generator qg1 = new Quiz_Generator();
 		qg1.create_quiestions();
+		//******************
 		
+		Player p1 = new Player();
+		System.out.print("Input name: ");
+		p1.setName(Scr.inStr());
+		p1.player_stats();
 		while(tmp != "q") {
-			tmp = Scr.inStr();
 			System.out.println("Game Menu");
 			System.out.println("-----------------------\n");
 			System.out.println("Start game, Y/N: ");
+			tmp = Scr.inStr();
 			qg1.publish_question();
+
 			if(tmp.equalsIgnoreCase("q")) {
 				System.out.println("The End!");
 				break;
