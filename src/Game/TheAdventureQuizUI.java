db1b99cb431932da1d8232d862bbf337d10299fb
Chjun-chi Chiu <chjunchi.chiu@gmail.com>
2019-01-28 15:06:17 +0100

    test

--- src/Game/TheAdventureQuizUI.java
+++ src/Game/TheAdventureQuizUI.java
@@ -1,40 +1,67 @@
 package Game;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import org.json.simple.parser.ParseException;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class TheAdventureQuizUI extends Application{
+public class TheAdventureQuizUI extends Application {
 
-	
-	    public static void main(String[] args) {
-	    	launch(args);   
-	    }
-	    
-	    @Override
-	    public void start(Stage primaryStage) {
-	        primaryStage.setTitle("Hello World!");
-	        Button btn = new Button();
-	        btn.setText("Say 'Hello World'");
-	        btn.setOnAction(new EventHandler<ActionEvent>() {
-	 
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Hello World!");
-	            }
-	        });
-	        
-	        StackPane root = new StackPane();
-	        root.getChildren().add(btn);
-	        primaryStage.setScene(new Scene(root, 300, 250));
-	        primaryStage.show();
-	    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws FileNotFoundException, IOException, ParseException {
+		primaryStage.setTitle("The Adventure Quiz Game!");
+
+		// Generate Questions
+		Quiz_Generator qg1 = new Quiz_Generator();
+		Quiz_Data data = new Quiz_Data();
+
+		data.quizBank(qg1);
+
+		// ******************
+		Text t = new Text();
+		Button btn = new Button();
+		qg1.publish_question();
+		t.setText(qg1.getActiveQuiz().getQuestion());
+		t.setLayoutX(100);
+		t.setLayoutY(100);
+		btn.setText("Say 'Hello World'");
+		btn.setLayoutX(200);
+		btn.setLayoutY(200);
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				qg1.publish_question();
+				t.setText(qg1.getActiveQuiz().getQuestion());
+				primaryStage.show();
+			}
+		});
+
+		Pane root = new Pane();
+
+		root.getChildren().add(btn);
+		root.getChildren().add(t);
+
+		primaryStage.setScene(new Scene(root, 1000, 700));
+		primaryStage.show();
+
+	}
 	
+	public static void name() {
+		
+	}
+
 }
